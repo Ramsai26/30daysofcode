0d06d20049d82122877cb528e6e29b12853a027f
Ramsai26 <chrs26081998@gmail.com>
2022-06-13 21:28:54 +0200

    updated_ 30 Days challenge

---
+++ code/day6_review.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+if __name__ == '__main__':
+    T=int(input())
+    for i in range(0,T):
+        S=input()
+        print(S[0::2]+" "+S[1::2])          
+            
